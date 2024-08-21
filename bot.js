7b049afe905feb5d8b615600ffd47e23d1e3aabd
Ilham Oktavian <99379666+Ilhamokta74@users.noreply.github.com>
2024-08-21 13:13:19 +0700

    fix

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 const fs = require('fs');
+const { finished } = require('stream');
 
 // URL dari halaman yang ingin di-scrape
 const url = 'https://www.dicoding.com/academies/list';
@@ -16,18 +17,24 @@ async function scrapeCourseCards() {
         // Array untuk menyimpan data yang di-scrape
         const courses = [];
 
-        // Ambil semua tag <a> dengan class "course-card course-card--with-logo"
-        $('a.course-card.course-card--with-logo').each((i, element) => {
-            const courseHref = $(element).attr('href'); // Mendapatkan atribut href
-            const courseLabel = $(element).attr('data-event-label'); // Mendapatkan atribut data-event-label
-
-            // Tambahkan data ke array
-            courses.push({
-                url: courseHref,
-                label: courseLabel,
-                finished: false
-            });
-        });
+                // Ambil semua tag <a> dengan class "course-card course-card--with-logo"
+                $('a.course-card.course-card--with-logo').each((i, element) => {
+                    const courseHref = $(element).attr('href'); // Mendapatkan atribut href
+                    const courseLabel = $(element).attr('data-event-label'); // Mendapatkan atribut data-event-label
+                    
+                    // Cari tag <img> di dalam elemen <a>
+                    const imgElement = $(element).find('img.course-card__logo');
+                    const imgSrc = imgElement.attr('src'); // Mendapatkan atribut src dari <img>
+                    const imgDataOriginal = imgElement.attr('data-original'); // Mendapatkan atribut data-original dari <img>
+        
+                    // Tambahkan data ke array
+                    courses.push({
+                        url: courseHref,
+                        label: courseLabel,
+                        imageUrl: imgDataOriginal,
+                        finished: false
+                    });
+                });
 
         // Simpan data ke file JSON
         fs.writeFileSync('./data/Data.json', JSON.stringify(courses, null, 2));
